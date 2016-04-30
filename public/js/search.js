0f09a611c96d77fbb16c299704153c4d2caa2bff
ajleon95 <ajl2217@columbia.edu>
2016-04-29 22:14:01 -0400

    Added highlight for search feature

--- public/js/search.js
+++ public/js/search.js
@@ -7,7 +7,6 @@
 
 var index = elasticlunr(function () {
   this.addField('title')
-  this.addField('author')
   this.addField('layout')
   this.addField('content')
   this.setRef('id')
@@ -19,7 +18,6 @@ var index = elasticlunr(function () {
 {% assign count = 0 %}{% for text in site.posts %}{% if text.mode == "annotated" %}
 index.addDoc({
   title: {{text.title | jsonify}},
-  author: {{text.author | jsonify}},
   layout: {{text.layout | jsonify}},
   content: {{text.content | jsonify | strip_html}},
   id: {{count}}
@@ -27,7 +25,6 @@ index.addDoc({
 {% for text in site.pages %}{% if text.materiales == true %}
 index.addDoc({
   title: {{text.title | jsonify}},
-  author: {{text.author | jsonify}},
   layout: {{text.layout | jsonify}},
   content: {{text.content | jsonify | strip_html}},
   id: {{count}}
@@ -39,34 +36,52 @@ console.log( jQuery.type(index) );
 
 var store = [{% for text in site.posts %}{% if text.mode == "annotated" %}{
   "title": {{text.title | jsonify}},
-  "author": {{text.author | jsonify}},
   "layout": {{ text.layout | jsonify }},
   "link": {{text.url | jsonify}},
-}
-,{% endif %}{% endfor %}{% for text in site.pages %}{% if text.materiales == true %}{
+},
+{% endif %}{% endfor %}{% assign materiales = site.pages | where: "materiales", true %}{% for text in materiales %}{
   "title": {{text.title | jsonify}},
-  "author": {{text.author | jsonify}},
   "layout": {{ text.layout | jsonify }},
   "link": {{text.url | jsonify}},
-}
-{% unless forloop.last %},{% endunless %}{% endif %}{% endfor %}]
+}{% unless forloop.last %},{% endunless %}
+{% endfor %}]
 
 //Query
 
-$(document).ready(function() {
-  $('input#search').on('keyup', function () {
-	var resultdiv = $('#results');
-	var query = $(this).val();
+var qd = {}; //Gets values from the URL
+location.search.substr(1).split("&").forEach(function(item) {
+    var s = item.split("="),
+        k = s[0],
+        v = s[1] && decodeURIComponent(s[1]);
+    (k in qd) ? qd[k].push(v) : qd[k] = [v]
+});
+
+function doSearch() {
+  var resultdiv = $('#results');
+  var query = $('input#search').val();
 
   //The search is then launched on the index built with Lunr
   var result = index.search(query);
   resultdiv.empty();
-  resultdiv.append('<p class="">Found '+result.length+' result(s)</p>');
+  if (result.length == 0) {
+    resultdiv.append('<p class="">No results found.</p>');
+  } else if (result.length == 1) {
+    resultdiv.append('<p class="">Found '+result.length+' result</p>');
+  } else {
+    resultdiv.append('<p class="">Found '+result.length+' results</p>');
+  }
   //Loop through, match, and add results
   for (var item in result) {
-	var ref = result[item].ref;
-    var searchitem = '<div class="result"><p><a href="{{ site.baseurl }}'+store[ref].link+'">'+store[ref].title+'</a></p></div>';
+    var ref = result[item].ref;
+    var searchitem = '<div class="result"><p><a href="{{ site.baseurl }}'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a></p></div>';
     resultdiv.append(searchitem);
-   }
-  });
+  }
+}
+
+$(document).ready(function() {
+  if (qd.q) {
+    $('input#search').val(qd.q[0]);
+    doSearch();
+  }
+  $('input#search').on('keyup', doSearch);
 });
